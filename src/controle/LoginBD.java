a2c69a66b714cb33d968f89d66793b2cd938f327
Julia de Almeida Jorge <juliadealmeida03@gmail.com>
2022-08-18 09:10:20 -0300

    Estrutura base

---
+++ src/controle/LoginBD.java
@@ -0,0 +1,12 @@
+package controle;
+
+import modelo.Usuario;
+
+public class LoginBD {
+
+	public boolean efetuarLogin(Usuario usuario) {
+
+		return false;
+	}
+
+}
---
+++ src/modelo/Usuario.java
@@ -0,0 +1,31 @@
+package modelo;
+
+/**
+ * Classe modelo para usuario que manipula o sistema
+ * 
+ * @author Julia
+ *
+ */
+
+public class Usuario {
+
+	private String emailAcesso;
+	private String senha;
+
+	public String getEmailAcesso() {
+		return emailAcesso;
+	}
+
+	public void setEmailAcesso(String emailAcesso) {
+		this.emailAcesso = emailAcesso;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
