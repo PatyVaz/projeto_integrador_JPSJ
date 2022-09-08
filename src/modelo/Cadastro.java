9ccfbe0d50bd2573a58156eeff28b618904c0457
Jorge Corrêa <jorge8cra@gmail.com>
2022-09-08 10:15:36 -0300

    Modelos atualizados

--- src/modelo/Cadastro.java
+++ src/modelo/Cadastro.java
@@ -2,9 +2,70 @@ package modelo;
 
 public class Cadastro {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int id;
+	private String nome;
+	private int cpf;
+	private int email;
+	private String rua;
+	private String bairro;
+	private int telefone;
+	private String cep;
+	private String cidade;
+	public int getId() {
+		return id;
 	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getEmail() {
+		return email;
+	}
+	public void setEmail(int email) {
+		this.email = email;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+
 
 }
