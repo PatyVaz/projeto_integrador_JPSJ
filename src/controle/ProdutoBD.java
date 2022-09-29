1f9c36ee9ec46eab974593052fad7852cd5a6506
Jorge Lucas Corrêa <jorge8cra@gmail.com>
2022-09-29 10:57:12 -0300

    ProdutoBD arrumado

---
+++ src/controle/ProdutoBD.java
@@ -0,0 +1,132 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.CadastroProdutos;
+
+public class ProdutoBD {
+	private Connection conexao;
+	public ProdutoBD() {
+		conexao = ConexaoBD.ConexaoBanco();
+	}
+
+	public int removeProduto(CadastroProdutos cadastroProdutos) {
+		
+		
+		
+		
+		try {
+
+			PreparedStatement ps = conexao.prepareStatement("delete from cadastroprodutos where id_cadastro=?");
+			ps.setInt(1, cadastroProdutos.getId());
+			return ps.executeUpdate();
+
+		} catch (SQLException e1) {
+			System.out.println("Erro ao conectar ï¿½ base de dados.");
+		}
+		return 0;
+	}
+	
+	
+	
+	
+	
+	
+
+	public ArrayList<CadastroProdutos> listarTodosProdutos() {
+		PreparedStatement ps;
+		ResultSet rs;
+		ArrayList<CadastroProdutos> listaProdutos = new ArrayList<CadastroProdutos>();
+		try {
+			ps = conexao.prepareStatement ("select * from cadastroprodutos order by modelo");
+			rs= ps.executeQuery();
+			while(rs.next( ) ) {
+				CadastroProdutos cadastroprodutos = new CadastroProdutos();
+				cadastroprodutos.setId(rs.getInt("id_produto"));
+				cadastroprodutos.setCor(rs.getString("cor"));
+				cadastroprodutos.setTamanho(rs.getInt("tamanho"));
+				cadastroprodutos.setMarca(rs.getString("marca"));
+				cadastroprodutos.setModelo(rs.getString("modelo"));
+				cadastroprodutos.setPreco(rs.getDouble("preco"));
+				
+				listaProdutos.add(cadastroprodutos);
+		}
+		} catch (SQLException e) {	
+			e.printStackTrace();
+		}
+		return listaProdutos;
+	}
+	
+	
+	
+	
+	
+
+	public boolean inserirProduto(CadastroProdutos cadastroProdutos) {
+	
+		try {
+
+			PreparedStatement ps = conexao.prepareStatement(
+					"insert into cadastroprodutos(cor, tamanho, marca, modelo, preco) values(?,?,?,?,?)");
+			
+			ps.setString(1, cadastroProdutos.getCor());
+			ps.setInt(2, cadastroProdutos.getTamanho());
+			ps.setString(3, cadastroProdutos.getMarca());
+			ps.setString(4, cadastroProdutos.getModelo());
+			ps.setDouble(5, cadastroProdutos.getPreco());
+			
+			ps.executeUpdate();
+
+		} catch (SQLException e1) {
+			System.out.println(e1);
+		}
+
+		return false;
+	}
+	
+	public int alterarProduto(CadastroProdutos cadastroProdutos) {
+	
+		try {
+			
+
+			PreparedStatement ps = conexao.prepareStatement("update cadastroprodutos set cor=? where id_produto = ?");
+			ps.setString(1, cadastroProdutos.getCor());
+			ps.setInt(2, cadastroProdutos.getId());
+			ps.executeUpdate();
+
+			ps.executeUpdate();
+			ps = conexao.prepareStatement("update cadastroprodutos set tamanho=? where id_produto = ?");
+			ps.setInt(1, cadastroProdutos.getTamanho());
+			ps.setInt(2, cadastroProdutos.getId());
+			ps.executeUpdate();
+
+			ps = conexao.prepareStatement("update cadastroprodutos set marca=? where id_produto = ?");
+			ps.setString(1, cadastroProdutos.getMarca());
+			ps.setInt(2, cadastroProdutos.getId());
+			ps.executeUpdate();
+
+			ps = conexao.prepareStatement("update cadastroprodutos set modelo=? where id_produto = ?");
+			ps.setString(1, cadastroProdutos.getModelo());
+			ps.setInt(2, cadastroProdutos.getId());
+			ps.executeUpdate();
+			
+			ps = conexao.prepareStatement("update cadastroprodutos set preco=? where id_produto = ?");
+			ps.setDouble(1, cadastroProdutos.getPreco());
+			ps.setInt(2, cadastroProdutos.getId());
+			ps.executeUpdate();
+			
+		
+			ps.executeUpdate();
+
+		} catch (SQLException e1) {
+
+			e1.printStackTrace();
+		}
+		return 0;
+	}
+
+}
