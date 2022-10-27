4f4d4c414fc8b0ee1ddb6f86be280e9391525f71
samuel <Samuelkayser123456789@gmail.com>
2022-10-27 00:20:14 -0300

    relatorio

---
+++ src/visao/TabelaRelatorioUsuario.java
@@ -0,0 +1,111 @@
+package visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+
+
+import modelo.Usuario;
+import java.awt.GridLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.table.DefaultTableModel;
+
+import controle.UsuarioBD;
+
+public class TabelaRelatorioUsuario extends JFrame {
+	
+	static Connection conexao;
+	private JPanel contentPane;
+	private JTable tbUsuario;
+	private ArrayList<Usuario> listaUsuarios;
+	private DefaultTableModel modelo;
+	protected static final int posicaoPessoa = 0;
+
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public TabelaRelatorioUsuario(TelaRelatorio TR ){
+		
+		
+			System.out.println(conexao);
+			
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		tbUsuario = new JTable();
+		tbUsuario.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"ID vendedor", "Login", "nome"
+			}
+			
+		));
+		scrollPane.setViewportView(tbUsuario);
+		UsuarioBD usuarioBD = new UsuarioBD();
+		listaUsuarios = usuarioBD.buscarUsuarios();
+		
+		tbUsuario.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "ID vendedor", "Login", "nome"}));
+		scrollPane.setViewportView(tbUsuario);
+
+		modelo = (DefaultTableModel) tbUsuario.getModel();
+		for (int i = 0; i < listaUsuarios.size(); i++) {
+			Usuario p = listaUsuarios.get(i);
+		modelo.addRow(new Object[] { p.getId(), p.getLogin(), p.getNome() });
+
+		}
+		tbUsuario.setModel(modelo);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1);
+		
+		JButton btnNewButton = new JButton("Selecionar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+int posicaoPessoa = tbUsuario.getSelectedRow();
+				
+				if(posicaoPessoa > -1) {
+					
+					
+					TR.txtVendedor.setText(tbUsuario.getValueAt(tbUsuario.getSelectedRow(), 0).toString());
+					
+					
+					TR.setVisible(true);
+					setVisible(false);				
+					}else {
+					JOptionPane.showMessageDialog(null,"escolha uma linha na tabela");
+					}
+			}
+			
+		});
+		panel_1.add(btnNewButton);
+	}
+
+}
