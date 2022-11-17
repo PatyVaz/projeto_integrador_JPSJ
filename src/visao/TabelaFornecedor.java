684206222b0d8d9932f55f1c6711db874739ddb7
Julia de Almeida Jorge <juliadealmeida03@gmail.com>
2022-11-17 07:57:16 -0300

    Configuracao tabela fornecedor

--- src/visao/TabelaFornecedor.java
+++ src/visao/TabelaFornecedor.java
@@ -1,41 +1,122 @@
 package visao;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JScrollPane;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import controle.FornecedorBD;
+import modelo.Fornecedor;
+import modelo.Usuario;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
 
 public class TabelaFornecedor extends JFrame {
+	public TabelaFornecedor() {
+	}
 
+	
+	static Connection conexao;
 	private JPanel contentPane;
+	private JTable tbFornecedor;
+	private ArrayList<Fornecedor> listaFornecedor;
+	private ArrayList<Fornecedor> listarTodosFornecedor;
+	private DefaultTableModel modelo;
+	protected static final int posicaoPessoa = 0;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TabelaFornecedor frame = new TabelaFornecedor();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
 	 */
-	public TabelaFornecedor() {
+	public TabelaFornecedor( ProdutosFornecedores pf ){
+		setTitle("Vendas do Usuário");
+		
+		
+			System.out.println(conexao);
+			
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane);
+		
+		tbFornecedor = new JTable();
+		tbFornecedor.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+					"ID", "Modelo", "Cor", "Tamanho", "Marca"
+			}
+			
+		));
+		scrollPane.setViewportView(tbFornecedor);
+		FornecedorBD fornecedorBD = new FornecedorBD();
+		listaFornecedor = fornecedorBD.listarTodosFornecedor();
+		
+		tbFornecedor.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "ID","Nome", "CNPJ"}));
+		scrollPane.setViewportView(tbFornecedor);
+
+		modelo = (DefaultTableModel) tbFornecedor.getModel();
+		for (int i = 0; i < listaFornecedor.size(); i++) {
+			Fornecedor f = listarTodosFornecedor.get(i);
+		modelo.addRow(new Object[] { f.getId(), f.getNome(), f.getCnpj() });
+
+		}
+		tbFornecedor.setModel(modelo);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		contentPane.add(panel_1);
+		
+		JButton btnNewButton = new JButton("Selecionar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+int posicaoPessoa = tbFornecedor.getSelectedRow();
+				
+				if(posicaoPessoa > -1) {
+					
+					
+					pf.textField.setText(tbFornecedor.getValueAt(tbFornecedor.getSelectedRow(), 0).toString());
+					
+					
+					pf.setVisible(true);
+					setVisible(false);				
+					}else {
+					JOptionPane.showMessageDialog(null,"escolha uma linha na tabela");
+					}
+			}
+			
+		});
+		panel_1.add(btnNewButton);
 	}
 
 }
